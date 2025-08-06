67fc4e329ad301a9f60a58cb509f383f7a18f295
Cristian Villalobos <cristianvillalobosvv@gmail.com>
2025-08-05 20:56:08 -0400

    creando app registro estudiantes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Python_curso
+
+# Ejercicios de logica de python.
---
+++ registro.py
@@ -0,0 +1,42 @@
+# Caso de estudio: registro de estudiantes en un curso
+
+# La institución "TechFormación" desea automatizar el registro de estudiantes inscritos
+# en sus cursos. Se solicita crear un programa en Python que permita:
+
+    # 1. Registrar estudiantes ingresados por nombre.
+    # 2. Mostrar todos los estudiantes inscritos.
+    # 3. Eliminar un estudiante por nombre.
+    # 4. Mostrar la cantidad total de inscritos.
+
+# Objetivo
+
+# Crear una clase en Python que utilice listas para almacenar y gestionar información de
+# estudiantes, permitiendo agregar nuevos registros y mostrar la lista de inscritos. El
+# estudiante aplicará conceptos de listas, atributos de clase, métodos y estructuras de
+# control.
+
+
+# Logica #
+
+# Definir una clase llamada Curso
+# Definir el metodo constructor, recive como parametros nombre
+# inicializar nombre y estudiantes, este ultimo con un array vacio
+
+# Getter y Setter #
+
+# Definimos funciones o metodos para:
+# Traer nombre
+# Modificar nombre
+# Traer estudiantes
+# Modificar estudiantes
+
+# Metodos de funcionalidad #
+
+# Definimos:
+# Agregar estudiantes
+# Eliminar estudiantes
+# Mostrar estudiantes
+# total inscritos
+
+# Ejecutar el programa
+
